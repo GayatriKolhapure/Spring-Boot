c2e08593ddaef5a0971bbe4b3f33cb434caa9c4b
Gayatri_Kolhapure <kgayatri1604@gmail.com>
2025-08-23 15:44:49 +0530

    4th Setter injections

--- src/main/java/org/gayatri/Alien.java
+++ src/main/java/org/gayatri/Alien.java
@@ -1,10 +1,22 @@
 package org.gayatri;
 
 
+import java.beans.ConstructorProperties;
+
 public class Alien {
 
     private int age;
     private Laptop laptop;
+
+    public Alien(){
+        System.out.println("Object created for Alien");
+    }
+    @ConstructorProperties({"age", "lap"})
+    public Alien(int age, Laptop laptop) {
+        System.out.println("Para constructor called");
+        this.age = age;
+        this.laptop = laptop;
+    }
 
     public int getAge() {
         return age;
@@ -23,9 +35,6 @@ public class Alien {
         this.laptop = laptop;
     }
 
-    public Alien(){
-        System.out.println("Object created for Alien");
-    }
     public void code(){
         System.out.println("Coding........");
         laptop.compile();
